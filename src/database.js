087600172e2b007250b63fb6eb16d733d3dc30bc
Erick <escoem84@gmail.com>
2021-04-06 00:29:43 -0500

    update

--- src/database.js
+++ src/database.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 
 // Direccion de la base de datos
 // si la base de datos no existe mongo la crea por nosotros
-const URI = "mongodb+srv://erick:288742@cluster0.dofmb.mongodb.net/mern-tasks?authSource=admin?retryWrites=true&w=majority";
+const URI = "mongodb+srv://erick:288742@cluster0.dofmb.mongodb.net/mern-tasks?retryWrites=true&w=majority";
 
-mongoose.connect(URI)
+mongoose.connect(URI,{useNewUrlParser: true})
 .then(db => console.log('DB is connected'))
 .catch(err => console.log(err));
 
