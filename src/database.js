1b5771267f410e928fc4cafb32557d322ab7638f
Erick <escoem84@gmail.com>
2021-04-06 01:29:45 -0500

    s

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 // si la base de datos no existe mongo la crea por nosotros
 const URI = "mongodb+srv://erick:288742@cluster0.dofmb.mongodb.net/mern-tasks?retryWrites=true&w=majority";
 
-mongoose.connect(URI)
+mongoose.connect(URI,{useNewUrlParser: true})
 .then(db => console.log('DB is connected'))
 .catch(err => console.log(err));
 
